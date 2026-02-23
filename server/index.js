d6818ba0f7a9004e46ece92932931a6123e5b208
Stargazer2026 <258677952+Stargazer2026@users.noreply.github.com>
2026-02-23 11:43:39 +0100

    Add dedicated /portrait-tv portrait display.

--- server/index.js
+++ server/index.js
@@ -181,6 +181,9 @@ app.use(express.static(__dirname + "/public"));
 app.get("/tv", limiter, function (req, res) { // TV Mode
     res.sendFile(__dirname + "/public/tv.html");
 });
+app.get("/portrait-tv", limiter, function (req, res) { // Portrait TV Mode
+    res.sendFile(__dirname + "/public/portrait-tv.html");
+});
 app.get("/debug", limiter, function (req, res) { // Debug page
     res.sendFile(__dirname + "/public/debug.html");
 });
